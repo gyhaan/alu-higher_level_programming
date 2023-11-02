638f3e2d97a297d033c3800c0966242177c2a77c
gyhaan <gyhaan@alustudent.com>
2023-11-02 15:51:02 +0000

    hello

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(str(c)) <= 97 and ord(str(c)) >= 122:
+    if ord(str(c)) >= 97 and ord(str(c)) <= 122:
         return True
     else:
         return False
