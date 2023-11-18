0d3176e3239578a2862743b493c1aeb7710e92ba
gyhaan <gyhaan@alustudent.com>
2023-11-18 11:50:24 +0000

    hello

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Defines a class of a square"""
-class Square:
-    """Presents a square """
-    pass
-=======
-"""Defines a class Square"""
+"""creates empty class Square"""
 
->>>>>>> 6682f991895049a3065f69d5741fd6563aa20a1b
 
 class Square:
-    """This is a square"""
+    """defines empty class."""
+
     pass
