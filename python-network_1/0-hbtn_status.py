5192e2c14f50b95eb8104c68257154b71a4964c4
gyhaan <g.yhaan@alustudent.com>
2023-12-01 18:12:04 +0000

    hello

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Documented"""
+"""This script fetches data from
+    https://alu-intranet.hbtn.io/status
+"""
+
+
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
-    '\n    Chrome/99.0.4844.84 Safari/537.36',
-}
-
-req = urllib.request.Request(url, headers=headers)
-with urllib.request.urlopen(req) as response:
-    content = response.read()
+with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+    body = response.read()
     print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
