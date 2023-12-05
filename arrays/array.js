29b615bd5d8d068b1d7be8943e7b164947b7425d
gyhaan <144003379+gyhaan@users.noreply.github.com>
2023-12-05 20:00:34 +0200

    array method

--- arrays/array.js
+++ arrays/array.js
@@ -7,7 +7,6 @@ console.log(array1.every(isBelowThreshold));
 const animals = ["Dodo", "Tiger", "Penguin", "Dodo"];
 
 console.log(animals.lastIndexOf("Dodo"));
-// Expected output: 3
 
 console.log(animals.lastIndexOf("Tiger"));
 // Expected output: 1
