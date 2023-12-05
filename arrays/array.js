b699aeb5cd4821cc0089c253350f119fe5281fa4
gyhaan <144003379+gyhaan@users.noreply.github.com>
2023-12-05 20:00:21 +0200

    code refactor

--- arrays/array.js
+++ arrays/array.js
@@ -3,3 +3,11 @@ const isBelowThreshold = (currentValue) => currentValue < 40;
 const array1 = [1, 30, 39, 29, 10, 13];
 
 console.log(array1.every(isBelowThreshold));
+
+const animals = ["Dodo", "Tiger", "Penguin", "Dodo"];
+
+console.log(animals.lastIndexOf("Dodo"));
+// Expected output: 3
+
+console.log(animals.lastIndexOf("Tiger"));
+// Expected output: 1
