36f14fe46c9e6ead39463b7ef8017eb25cc3a421
gyhaan <144003379+gyhaan@users.noreply.github.com>
2023-12-12 16:58:02 +0200

    line indent

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,7 +12,6 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
     c = 0
     while c < len(text) and text[c] == ' ':
         c += 1
