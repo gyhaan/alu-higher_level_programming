4ee376d2d1d65829847e5e1d490bf370e6548cb2
gyhaan <144003379+gyhaan@users.noreply.github.com>
2023-12-17 19:14:16 +0200

    21

--- arrays/array.js
+++ arrays/array.js
@@ -1,6 +1,6 @@
 const isBelowThreshold = (currentValue) => currentValue < 40;
 
-const array1 = [1, 30, 39, 29, 20];
+const array1 = [1, 30, 39, 29, 20, 21];
 
 function getMinMax(arr) {
   const newarr = arr.slice().sort((a, b) => a - b);
