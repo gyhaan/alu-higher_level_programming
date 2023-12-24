9c0f85b79ab056f018b7d1b7124057c8208c1f5d
gyhaan <144003379+gyhaan@users.noreply.github.com>
2023-12-24 21:05:53 +0200

    code refactor

--- arrays/array.js
+++ arrays/array.js
@@ -11,10 +11,6 @@ const array1 = ["a", "b", "c"];
 
 array1.forEach((element) => console.log(element));
 
-// Expected output: "a"
-// Expected output: "b"
-// Expected output: "c"
-
 /* Algos */
 
 /* Q1 */
