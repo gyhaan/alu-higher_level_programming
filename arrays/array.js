c234adc6b9438b767b4ac1951a67ae8137a262f4
gyhaan <144003379+gyhaan@users.noreply.github.com>
2023-12-29 19:49:44 +0200

    code refactor

--- arrays/array.js
+++ arrays/array.js
@@ -12,9 +12,7 @@ console.log("array1:", array1);
 
 const reversed = array1.reverse();
 console.log("reversed:", reversed);
-// Expected output: "reversed:" Array ["three", "two", "one"]
 
-// Careful: reverse is destructive -- it changes the original array.
 console.log("array1:", array1);
 // Expected output: "array1:" Array ["three", "two", "one"]
 
