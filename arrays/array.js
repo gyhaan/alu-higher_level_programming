1242e2f822bc435125937c060f901dc4d32a1a87
gyhaan <144003379+gyhaan@users.noreply.github.com>
2023-12-29 19:49:28 +0200

    remove comment

--- arrays/array.js
+++ arrays/array.js
@@ -9,7 +9,6 @@ function getMinMax(arr) {
 
 const array1 = ["one", "two", "three"];
 console.log("array1:", array1);
-// Expected output: "array1:" Array ["one", "two", "three"]
 
 const reversed = array1.reverse();
 console.log("reversed:", reversed);
