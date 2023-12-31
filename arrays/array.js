de8a7ea529ad296e47766f748725fd9ff4b2373f
gyhaan <144003379+gyhaan@users.noreply.github.com>
2023-12-31 15:19:49 +0200

    code removal

--- arrays/array.js
+++ arrays/array.js
@@ -8,12 +8,6 @@ function getMinMax(arr) {
 }
 
 const array1 = ["one", "two", "three"];
-console.log("array1:", array1);
-
-const reversed = array1.reverse();
-console.log("reversed:", reversed);
-
-console.log("array1:", array1);
 
 /* Algos */
 
