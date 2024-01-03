770ad9d323d2d6bcc505a6b68ffaf0af323dbfd1
gyhaan <144003379+gyhaan@users.noreply.github.com>
2024-01-03 22:51:17 +0200

    move zeroes

--- arrays/array.js
+++ arrays/array.js
@@ -209,3 +209,16 @@ function validIP(str) {
   }
   return "Valid IPv4 address";
 }
+
+/* Q12 */
+function moveZeroes(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      arr.splice(i, 1);
+      arr.push(0);
+    } else {
+      continue;
+    }
+  }
+  return arr;
+}
