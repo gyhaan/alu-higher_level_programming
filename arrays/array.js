bf87f5603325f6026a51fbcfb48813c14fbf4347
gyhaan <144003379+gyhaan@users.noreply.github.com>
2024-01-11 12:41:37 +0200

    single Number leetcode

--- arrays/array.js
+++ arrays/array.js
@@ -310,3 +310,12 @@ function twoSum(arr, value) {
     }
   }
 }
+
+/* Q21 */
+function singleNumber(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.indexOf(arr[i]) === i && arr.lastIndexOf(arr[i]) === i) {
+      return arr[i];
+    }
+  }
+}
