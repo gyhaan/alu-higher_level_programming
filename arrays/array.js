804d97add1356a075b8898a43388506deb9a6fec
gyhaan <144003379+gyhaan@users.noreply.github.com>
2024-01-16 23:22:15 +0200

    checkDiff quick Algo

--- arrays/array.js
+++ arrays/array.js
@@ -345,3 +345,15 @@ function containsDuplicate(arr) {
   }
   return false;
 }
+
+/* Q24 */
+function checkDiff(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
